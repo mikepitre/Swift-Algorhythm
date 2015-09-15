e675ca872ed375b3a8e610dcb8518b46c96dd156
Mike Pitre <michaelepitre@gmail.com>
2015-09-15 11:39:32 -0500

    initial commit

---
+++ Algorhythm/PlaylistDetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  PlaylistDetailViewController.swift
+//  Algorhythm
+//
+//  Created by Mike Pitre on 9/15/15.
+//  Copyright © 2015 Mike Pitre. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
