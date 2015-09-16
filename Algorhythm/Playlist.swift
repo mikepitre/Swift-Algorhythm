386c3e6da54f5c629d3a4adfb247c5c8c48cf3b2
Mike Pitre <michaelepitre@gmail.com>
2015-09-15 22:45:01 -0500

    added musicLibrary and playlist structs

---
+++ Algorhythm/Playlist.swift
@@ -0,0 +1,20 @@
+//
+//  Playlist.swift
+//  Algorhythm
+//
+//  Created by Mike Pitre on 9/15/15.
+//  Copyright © 2015 Mike Pitre. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Playlist {
+    
+    var title: String?
+    var description: String?
+    var icon: UIImage?
+    var largeIcon: UIImage?
+    var artists: [String] = []
+    var backgroundColor: UIColor = UIColor.clearColor()
+}
